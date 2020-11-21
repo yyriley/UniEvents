2286f5666b453c16826fad9036a743026ad92291
Yrua <yyr7@txstate.edu>
2020-11-20 20:26:07 -0600

    created xcode project

---
+++ UniEvents/UniEvents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniEvents
+//
+//  Created by Yrua Riley on 11/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
