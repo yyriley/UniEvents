f2fe4d53b9e0729d70b2ec3e02f1e55662bb9770
Yrua <yyr7@txstate.edu>
2020-11-27 03:24:10 -0600

    Added HomeCell

---
+++ UniEvents/UniEvents/HomeCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeCell.swift
+//  UniEvents
+//
+//  Created by Yrua Riley on 11/27/20.
+//
+
+import UIKit
+
+class HomeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
