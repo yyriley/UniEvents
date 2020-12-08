cdd6f0fbf4ce49cc5c608a79b2556c0bf91a580d
Yrua <yyr7@txstate.edu>
2020-12-08 09:28:22 -0600

    added club cell

---
+++ UniEvents/UniEvents/Views/ClubCell.swift
@@ -0,0 +1,23 @@
+//
+//  ClubCell.swift
+//  UniEvents
+//
+//  Created by Yrua Riley on 12/8/20.
+//
+
+import UIKit
+
+class ClubCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
